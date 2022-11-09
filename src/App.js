5f709dbede9222a001587f59e4d0523a9ed58bdb
tinley <tinwai0708@gmail.com>
2022-11-09 20:16:57 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useLoader } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
 import './App.css';
+
+function Scene() {
+  const [colorMap, displacementMap, normalMap, roughnessMap, aoMap] = useLoader(TextureLoader, [
+    'test_normal.png'
+  ])
+  const obj = useLoader(OBJLoader, 'test.obj')
+  return (
+    <>
+      <ambientLight intensity={0.2} />
+      <directionalLight />
+      <mesh position={[1, 2, 3]} rotation={[Math.PI / 2, 0, 0]}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshStandardMaterial
+          map={colorMap}
+          displacementMap={displacementMap}
+          normalMap={normalMap}
+          roughnessMap={roughnessMap}
+          aoMap={aoMap}
+        />
+        <primitive object={obj} scale={1} />
+      </mesh>
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Scene />
+          <OrbitControls />
+        </Suspense>
+      </Canvas>
     </div>
-  );
+  )
 }
 
 export default App;
